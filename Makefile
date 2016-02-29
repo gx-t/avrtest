ba3830e8199d08078c3b92050b54046a3630dd51
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2016-02-29 11:14:20 +0400

    Added test02.c

--- Makefile
+++ Makefile
@@ -12,7 +12,13 @@ test01:
 	rm *.elf
 	avrdude -c USBASP -p t2313 -U flash:w:test01.hex -U lfuse:w:0xe6:m
 
-tags: *.c
+test02:
+	avr-gcc -mmcu=attiny2313 -Werror -Os -s test02.c -o test02.elf
+	avr-objcopy -j .text -j .data -O ihex test02.elf test02.hex
+	rm *.elf
+	avrdude -c USBASP -p t2313 -U flash:w:test02.hex -U lfuse:w:0xe6:m
+
+tags:
 	ctags -R . /usr/lib/avr/include/
 
 clean:
--- test00.c
+++ test00.c
@@ -11,18 +11,6 @@
 	Battery LiIon (3.6V)
 */
 
-static uint8_t b[7];
-ISR(TIMER0_COMPA_vect) {
-	static uint8_t i = 0;
-	PORTD = 0b1111111;
-	PORTB = b[i];
-	PORTD = ~(0b1 << i);
-	i ++;
-	if(i == 7) {
-		i = 0;
-	}
-}
-
 static void sys_init() {
 	PORTB = 0x0;
 	DDRB = 0xff;
@@ -34,6 +22,20 @@ static void sys_init() {
 	TIMSK = 0x01;
 	TCCR0B = 0x01;
 	sei();
+}
+
+
+static uint8_t b[7];
+
+ISR(TIMER0_COMPA_vect) {
+	static uint8_t i = 0;
+	PORTD = 0b1111111;
+	PORTB = b[i];
+	PORTD = ~(0b1 << i);
+	i ++;
+	if(i == 7) {
+		i = 0;
+	}
 }
 
 static void ff_s_l() {
@@ -60,34 +62,52 @@ static void ff_s_r() {
 	}
 }
 
-static void fast_bi() {
-	uint8_t rep1, rep, j, i;
-	rep1 = 2;
-	while(rep1 --) {
-		for(rep = 0; rep < 4; rep ++) {
-			for(j = 0; j < 7; j ++) {
-				for(i = 0; i < 8; i ++) {
-					b[j] = 1 << i;
-					__builtin_avr_delay_cycles(20000);
-					b[j] = 0;
-				}
-			}
-		}
-		while(rep --) {
-			j = 7;
-			while(j --) {
-				i = 8;
-				while(i --) {
-					b[j] = 1 << i;
-					__builtin_avr_delay_cycles(20000);
-					b[j] = 0;
-				}
+static void ff_s_bi() {
+	uint8_t i = 2;
+	while(i --) {
+		ff_s_l();
+		ff_s_r();
+	}
+}
+
+static void fast_l() {
+	uint8_t rep, j, i;
+	for(rep = 0; rep < 4; rep ++) {
+		for(j = 0; j < 7; j ++) {
+			for(i = 0; i < 8; i ++) {
+				b[j] = 1 << i;
+				__builtin_avr_delay_cycles(20000);
+				b[j] = 0;
 			}
 		}
 	}
 }
 
-static void fast_s_bi() {
+static void fast_r() {
+	uint8_t rep, j, i;
+	rep = 4;
+	while(rep --) {
+		j = 7;
+		while(j --) {
+			i = 8;
+			while(i --) {
+				b[j] = 1 << i;
+				__builtin_avr_delay_cycles(20000);
+				b[j] = 0;
+			}
+		}
+	}
+}
+
+static void fast_bi() {
+	uint8_t i = 2;
+	while(i --) {
+		fast_l();
+		fast_r();
+	}
+}
+
+static void fill_l() {
 	uint8_t rep, j, i;
 	rep = 4;
 	while(rep --) {
@@ -107,7 +127,7 @@ static void fast_s_bi() {
 	}
 }
 
-static void fill_bi() {
+static void fill_r() {
 	uint8_t rep, j, i;
 	rep = 4;
 	while(rep --) {
@@ -128,7 +148,7 @@ static void fill_bi() {
 	}
 }
 
-static void fill_s_bi() {
+static void fill_s_l() {
 	uint8_t rep, i;
 	rep = 4;
 	while(rep --) {
@@ -146,26 +166,49 @@ static void fill_s_bi() {
 	}
 }
 
-int main (void) {
-	uint8_t i;
-	sys_init();
-    while(1) {
-		fast_bi();
-		i = 2;
+static void fill_s_r() {
+	uint8_t rep, i;
+	rep = 4;
+	while(rep --) {
+		i = 8;
 		while(i --) {
-			ff_s_l();
-			ff_s_r();
+			b[6] |= 1 << i;
+			b[0] = b[1] = b[2] = b[3] = b[4] = b[5] = b[6];
+			__builtin_avr_delay_cycles(100000);
 		}
-		fast_s_bi();
-		ff_s_l();
-		fill_bi();
-		ff_s_r();
-		i = 2;
-		while(i --) {
-			fill_s_bi();
-			ff_s_l();
-			fill_s_bi();
-			ff_s_r();
+		for(i = 0; i < 8; i ++) {
+			b[6] &= ~(1 << i);
+			b[0] = b[1] = b[2] = b[3] = b[4] = b[5] = b[6];
+			__builtin_avr_delay_cycles(100000);
 		}
 	}
 }
+
+int main (void) {
+	sys_init();
+    while(1) {
+		fast_bi();
+		ff_s_bi();
+		fill_l();
+		ff_s_l();
+		fill_r();
+		ff_s_r();
+		fill_s_l();
+		ff_s_l();
+		fill_s_r();
+		ff_s_r();
+
+		fast_bi();
+		ff_s_bi();
+		fast_l();
+		fill_l();
+		fast_r();
+		fill_r();
+		fast_l();
+		fill_s_l();
+		fast_r();
+		fill_s_r();
+	}
+}
+
+
