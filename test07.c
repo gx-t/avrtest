641d70cc61af611ae6b8ce997ecae3347442806a
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2018-07-25 15:04:25 +0400

    Indents

--- test07.c
+++ test07.c
@@ -6,11 +6,11 @@
 #include <util/delay.h>
 
 /*
-ATMEGA 328P
-    RTC: 9-10 32768 Hz QZ 
-    9 -- 32768HZ -- 10
-    Serial in/out
-*/
+   ATMEGA 328P
+RTC: 9-10 32768 Hz QZ 
+9 -- 32768HZ -- 10
+Serial in/out
+ */
 
 ISR(TIMER2_OVF_vect)
 {
@@ -18,10 +18,10 @@ ISR(TIMER2_OVF_vect)
 
 static void rtc_init(void)
 {  
-	TCCR2A = 0x00;  //overflow
-	TCCR2B = 0x05;  //5 gives 1 sec. prescale 
-	TIMSK2 = 0x01;  //enable timer2A overflow interrupt
-	ASSR  = 0x20;   //enable asynchronous mode
+    TCCR2A = 0x00;  //overflow
+    TCCR2B = 0x05;  //5 gives 1 sec. prescale 
+    TIMSK2 = 0x01;  //enable timer2A overflow interrupt
+    ASSR  = 0x20;   //enable asynchronous mode
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -33,62 +33,60 @@ ISR(USART_RX_vect)
 }
 
 static void uart_init() {
-	UBRR0H = (uint8_t) (USART_UBBR_VALUE >> 8);
-	UBRR0L = (uint8_t) USART_UBBR_VALUE;
+    UBRR0H = (uint8_t) (USART_UBBR_VALUE >> 8);
+    UBRR0L = (uint8_t) USART_UBBR_VALUE;
 
-	UCSR0A = 0;
-	
+    UCSR0A = 0;
 
-	//Enable UART
-	UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
 
-	//8 data bits, 1 stop bit
-	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+    //Enable UART
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
+
+    //8 data bits, 1 stop bit
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
 static void uart_tx(uint8_t data)
 {
-	while (!(UCSR0A & (1 << UDRE0)));
-	UDR0 = data;
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = data;
 }
 
 static void p_line(const char* pp) {
-	while(*pp) {
-		uart_tx(*pp++);
-	}
-	uart_tx('\r');
-	uart_tx('\n');
+    while(*pp) {
+        uart_tx(*pp++);
+    }
+    uart_tx('\r');
+    uart_tx('\n');
 }
 
-///////////////////////////////////////////////////////////////////////////////
-//Interrupts
 
 static void sys_init() {
-	cli();
-	set_sleep_mode(SLEEP_MODE_IDLE);
-	sleep_enable();
-	uart_init();
-	rtc_init();
-	sei();
+    cli();
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    sleep_enable();
+    uart_init();
+    rtc_init();
+    sei();
 }
 
 int main(void) {
-	sys_init();
-	while(1) {
-		sleep_cpu();
-		if(UCSR0A & (1 << RXC0)) {
-			uint8_t ch = UDR0;
-			if(ch == '\r') {
+    sys_init();
+    while(1) {
+        sleep_cpu();
+        if(UCSR0A & (1 << RXC0)) {
+            uint8_t ch = UDR0;
+            if(ch == '\r') {
                 uart_tx('\n');
-			}
+            }
             else {
                 uart_tx(ch);
             }
-		}
-		else {
+        }
+        else {
             p_line("RTC");
-		}
-	} 
-	return 1;
+        }
+    } 
+    return 1;
 }
 
