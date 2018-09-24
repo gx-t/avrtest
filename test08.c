40dc8ed620b8c09611e2086595096c5c3dbde60b
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2018-09-24 10:30:21 +0400

    Binary values instead of separate functions

--- test08.c
+++ test08.c
@@ -265,45 +265,20 @@ static uint8_t lora_check_tx_done()
     return !!(0b0001000 & lora_read_reg(0x12));
 }
 
-static void adc_mux_avcc_11()
-{
-    ADMUX = 0b01001110; 
-}
-
-static void adc_mux_off_gnd()
-{
-    ADMUX = 0b00001111;
-}
-
-static void adc_en_sc_128()
-{
-    ADCSRA = 0b11000111;
-}
-
-static void adc_disable_128()
-{
-    ADCSRA = 0b00000111;
-}
-
-static void adc_wait_done()
-{
-    while(ADCSRA & (1 << ADSC));
-}
-
 static void adc_read_vcc()
 {
-    adc_mux_avcc_11();
-    adc_en_sc_128();
-    adc_wait_done();
-    adc_en_sc_128();
-    adc_wait_done();
+    ADMUX = 0b01001110; 
+    ADCSRA = 0b11000111;
+    while(ADCSRA & 0b01000000);
+    ADCSRA = 0b11000111;
+    while(ADCSRA & 0b01000000);
 
     //VCC = 1.1 * 1023 / read
     p_hex_digit(ADCL);
     p_hex_digit(ADCH);
+    ADMUX = 0b00001111;
+    ADCSRA = 0b00000111;
     p_line("");
-    adc_mux_off_gnd();
-    adc_disable_128();
 }
 
 static void f_tx()
