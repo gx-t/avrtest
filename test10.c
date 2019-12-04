3a75756320a72cbe295bf23672666d743831d0d8
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2019-12-04 23:39:06 +0400

    added random push effect

--- test10.c
+++ test10.c
@@ -1,24 +1,16 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <stdlib.h>
 
 #define F_CPU 8000000UL
 #include <util/delay.h>
 
-int main(void) {
-
-    int16_t s[2][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}};
-    int16_t c[2][4] = {{30 << 8, 30 << 8, 30 << 8, 30 << 8}, {30 << 8, 30 << 8, 30 << 8, 30 << 8}};
+static void effect_0()
+{
+    static int16_t s[2][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}};
+    static int16_t c[2][4] = {{30 << 8, 30 << 8, 30 << 8, 30 << 8}, {30 << 8, 30 << 8, 30 << 8, 30 << 8}};
     const int16_t p[2][4] = {{100, 101, 103, 107}, {101, 102, 104, 108}};
-
-    DDRD |= _BV(PD6) | _BV(PD5) | _BV(PD3);
-    DDRB |= _BV(PB3);
-
-    TCCR0A |= _BV(COM2A1) | _BV(COM0B1) | _BV(WGM00) | _BV(WGM01);
-    TCCR0B |= _BV(CS00);
-
-    TCCR2A |= _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20) | _BV(WGM21);
-    TCCR2B |= _BV(CS00);
 
     while(1) {
 
@@ -56,6 +48,50 @@ int main(void) {
         OCR2A = cc_0;
         OCR2B = cc_1;
     }
+}
+
+static void effect_1()
+{
+    static float s[4] = {0, 0, 0, 0};
+    static float c[4] = {127, 127, 127, 127};
+    while(1) {
+
+        int r = random();
+        for(uint8_t i = 0; i < sizeof(s) / sizeof(s[0]); i ++, r >>= 1) {
+            if(r & 1) {
+                s[i] = 0;
+                c[i] = 127;
+            }
+        }
+
+        for(uint16_t i = 0; i < 5000; i ++) {
+            for(uint8_t j = 0; j < sizeof(s) / sizeof(s[0]); j ++) {
+                c[j] -= s[j] / 64;
+                s[j] += c[j] / 64;
+                s[j] += (0 - s[j]) * 0.0005;
+            }
+
+            OCR0A = (uint8_t)(s[0] + 127);
+            OCR0B = (uint8_t)(s[1] + 127);
+            OCR2A = (uint8_t)(s[2] + 127);
+            OCR2B = (uint8_t)(s[3] + 127);
+        }
+    }
+}
+
+int main(void) {
+
+    DDRD |= _BV(PD6) | _BV(PD5) | _BV(PD3);
+    DDRB |= _BV(PB3);
+
+    TCCR0A |= _BV(COM2A1) | _BV(COM0B1) | _BV(WGM00) | _BV(WGM01);
+    TCCR0B |= _BV(CS00);
+
+    TCCR2A |= _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20) | _BV(WGM21);
+    TCCR2B |= _BV(CS00);
+
+    effect_0();
+    effect_1();
 
     return 0;
 }
