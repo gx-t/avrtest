be02f14a93e30ba583c3bab51a3ad421f8a753c9
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2020-03-07 14:47:49 +0400

    WDT instead of 32Khz quartz

--- test11.c
+++ test11.c
@@ -1,7 +1,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
-#include <stdio.h>
+#include <avr/wdt.h>
+//#include <stdio.h>
 
 #define F_CPU 8000000UL
 #include <util/delay.h>
@@ -17,45 +18,31 @@
     2.2uF 0805 7, 8
 */
 
-#define USART_BAUD 38400UL
-#define USART_UBBR_VALUE ((F_CPU / (USART_BAUD << 4)) - 1)
-
-static void uart_init() {
-    UBRR0H = (uint8_t) (USART_UBBR_VALUE >> 8);
-    UBRR0L = (uint8_t) USART_UBBR_VALUE;
-    UCSR0A = 0;
-    //Enable UART
-    UCSR0B = (1 << TXEN0);
-    //8 data bits, 1 stop bit
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-}
-
-static int uart_print_char(char ch, FILE* pf)
-{
-    while (!(UCSR0A & (1 << UDRE0)));
-    UDR0 = ch;
-    return 0;
-}
-
-static FILE uart_str = {
-    .put = uart_print_char,
-    .flags = _FDEV_SETUP_RW
-};
-
-//static void led_on()
-//{
-//    PORTD |= (_BV(PD6) | _BV(PD5) | _BV(PD3));
-//    PORTB |= (_BV(PB3));
-//    fprintf(&uart_str, "%s\r\n", __func__);
+//#define USART_BAUD 38400UL
+//#define USART_UBBR_VALUE ((F_CPU / (USART_BAUD << 4)) - 1)
+//
+//static void uart_init() {
+//    UBRR0H = (uint8_t) (USART_UBBR_VALUE >> 8);
+//    UBRR0L = (uint8_t) USART_UBBR_VALUE;
+//    UCSR0A = 0;
+//    //Enable UART
+//    UCSR0B = (1 << TXEN0);
+//    //8 data bits, 1 stop bit
+//    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 //}
 //
-//static void led_off()
+//static int uart_print_char(char ch, FILE* pf)
 //{
-//    PORTD &= ~(_BV(PD6) | _BV(PD5) | _BV(PD3));
-//    PORTB &= ~(_BV(PB3));
-//    fprintf(&uart_str, "%s\r\n", __func__);
+//    while (!(UCSR0A & (1 << UDRE0)));
+//    UDR0 = ch;
+//    return 0;
 //}
-
+//
+//static FILE uart_str = {
+//    .put = uart_print_char,
+//    .flags = _FDEV_SETUP_RW
+//};
+//
 static void reset_enable_pullup()
 {
     PORTC |= _BV(PC6);
@@ -67,16 +54,45 @@ static void led_enable_pin()
     DDRB |= _BV(PB3);
 }
 
-static void rtc_init()
-{  
-    TCCR2A = 0x00;  //overflow
-    TCCR2B = 0x05;  //5 gives 1 sec. prescale 
-    TIMSK2 = 0x01;  //enable timer2A overflow interrupt
-    ASSR  = 0x20;   //enable asynchronous mode
+//static void rtc_init()
+//{  
+//    TCCR2B = 0;  //stop Timer 2
+//    TIMSK2 = 0; // disable Timer 2 interrupts
+//    ASSR = (1<<AS2); // select asynchronous operation of Timer2
+//    TCNT2 = 0; // clear Timer 2 counter
+//    TCCR2A = 0; //normal count up mode, no port output
+//    TCCR2B = (1<<CS22) | (1<<CS20); // select prescaler 128 => 1 sec between each overflow
+//
+//    while (ASSR & ((1<<TCN2UB)|(1<<TCR2BUB))); // wait for TCN2UB and TCR2BUB to clear
+//
+//    TIFR2 = 0xFF; // clear all interrupt flags
+//    TIMSK2 = (1<<TOIE2); // enable Timer2 overflow interrupt
+//}
+
+static void wdt_init()
+{
+    wdt_reset();
+    WDTCSR = 0b00011000;
+    WDTCSR = 0b01001111;
 }
 
-ISR(TIMER2_OVF_vect)
+ISR(WDT_vect)
 {
+//    fprintf(&uart_str, "interrupt\r\n");
+}
+
+static void wdt_sleep_2s()
+{
+    WDTCSR = 0b00011000;
+    WDTCSR = 0b01001111;
+    sleep_cpu();
+}
+
+static void wdt_sleep_64ms()
+{
+    WDTCSR = 0b00011000;
+    WDTCSR = 0b01000011;
+    sleep_cpu();
 }
 
 static void sys_init()
@@ -84,46 +100,50 @@ static void sys_init()
     cli();
     reset_enable_pullup();
     led_enable_pin();
-    set_sleep_mode(SLEEP_MODE_IDLE);
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
-    rtc_init();
-    uart_init();
+    wdt_init();
+//    uart_init();
     sei();
 }
 
+static void led_0_on()  { PORTD |= _BV(PD6); }
+static void led_0_off() { PORTD &= ~_BV(PD6); }
+
+static void led_1_on()  { PORTB |= _BV(PB3); }
+static void led_1_off() { PORTB &= ~_BV(PB3); }
+
+static void led_2_on()  { PORTD |= _BV(PD5); }
+static void led_2_off() { PORTD &= ~_BV(PD5); }
+
+static void led_3_on()  { PORTD |= _BV(PD3); }
+static void led_3_off() { PORTD &= ~_BV(PD3); }
+
 int main(void) {
-    printf("%s\r\n", __func__);
     sys_init();
+//    fprintf(&uart_str, "main\r\n");
     while(1) {
-        PORTD |= _BV(PD6);
-        fprintf(&uart_str, "LED-0 on\r\n");
-        _delay_ms(5);
-        PORTD &= ~_BV(PD6);
-        fprintf(&uart_str, "LED-0 off\r\n");
-        _delay_ms(50);
-        PORTD |= _BV(PD5);
-        fprintf(&uart_str, "LED-1 on\r\n");
-        _delay_ms(5);
-        PORTD &= ~_BV(PD5);
-        fprintf(&uart_str, "LED-1 off\r\n");
-        _delay_ms(50);
-        PORTD |= _BV(PD3);
-        fprintf(&uart_str, "LED-2 on\r\n");
-        _delay_ms(5);
-        PORTD &= ~_BV(PD3);
-        fprintf(&uart_str, "LED-2 off\r\n");
-        _delay_ms(50);
-        PORTB |= _BV(PB3);
-        fprintf(&uart_str, "LED-3 on\r\n");
-        _delay_ms(5);
-        PORTB &= ~_BV(PB3);
-        fprintf(&uart_str, "LED-3 off\r\n");
-        //        led_on();
-        //        TCNT2 = 16;
-        //        sleep_cpu();
-        //        sleep_cpu();
-//        _delay_ms(1000);
-        sleep_cpu();
+        led_0_on();
+        _delay_ms(2);
+        led_0_off();
+        wdt_sleep_64ms();
+        led_1_on();
+        _delay_ms(2);
+        led_1_off();
+        wdt_sleep_64ms();
+        led_2_on();
+        _delay_ms(2);
+        led_2_off();
+        wdt_sleep_64ms();
+        led_3_on();
+        _delay_ms(2);
+        led_3_off();
+        wdt_sleep_64ms();
+        led_0_on();
+        _delay_ms(2);
+        led_0_off();
+        wdt_sleep_2s();
+//        fprintf(&uart_str, "wake\r\n");
     }
     return 0;
 }
