c9c987f08f2a73ce3f715ea760cb1fc1175408eb
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2020-03-14 17:55:21 +0400

    attiny85

--- Makefile
+++ Makefile
@@ -67,10 +67,10 @@ all:
 	avrdude -c USBASP -p m328p -U flash:w:test10.hex -U lfuse:w:0xe2:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
 
 11:
-	avr-gcc -mmcu=atmega328p -Wall -Werror -Os -s test11.c -o test11.elf
+	avr-gcc -mmcu=attiny85 -Wall -Werror -Os -s test11.c -o test11.elf
 	avr-objcopy -j .text -j .data -O ihex test11.elf test11.hex
 	rm *.elf
-	avrdude -c USBASP -p m328p -U flash:w:test11.hex -U lfuse:w:0xe2:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
+	avrdude -c USBASP -p t85 -U flash:w:test11.hex -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 
 client_rel:
 	gcc -O2 -Werror -s client.c -o client
--- test11.c
+++ test11.c
@@ -2,7 +2,6 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include <avr/wdt.h>
-//#include <stdio.h>
 
 #define F_CPU 8000000UL
 #include <util/delay.h>
@@ -10,140 +9,76 @@
 /*
     Solar cell + ultracapacitor powered LED beacon
 
-    ATMEGA328P
-    7, 8 4F
-    7 Schottky diode Solar +
-    8 Solar -
-    PB3, PD3, PD5, PD6   LED(n)+
-    2.2uF 0805 7, 8
+    ATTINY85
 */
 
-//#define USART_BAUD 38400UL
-//#define USART_UBBR_VALUE ((F_CPU / (USART_BAUD << 4)) - 1)
-//
-//static void uart_init() {
-//    UBRR0H = (uint8_t) (USART_UBBR_VALUE >> 8);
-//    UBRR0L = (uint8_t) USART_UBBR_VALUE;
-//    UCSR0A = 0;
-//    //Enable UART
-//    UCSR0B = (1 << TXEN0);
-//    //8 data bits, 1 stop bit
-//    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-//}
-//
-//static int uart_print_char(char ch, FILE* pf)
-//{
-//    while (!(UCSR0A & (1 << UDRE0)));
-//    UDR0 = ch;
-//    return 0;
-//}
-//
-//static FILE uart_str = {
-//    .put = uart_print_char,
-//    .flags = _FDEV_SETUP_RW
-//};
-//
-static void reset_enable_pullup()
+static void gpio_init()
 {
-    PORTC |= _BV(PC6);
+    DDRB = 0b00011011; //4 LEDs
+    PORTB = 0b00100000; //pull-up of "reset"
 }
-
-static void led_enable_pin()
-{
-    DDRD |= _BV(PD6) | _BV(PD5) | _BV(PD3);
-    DDRB |= _BV(PB3);
-}
-
-//static void rtc_init()
-//{  
-//    TCCR2B = 0;  //stop Timer 2
-//    TIMSK2 = 0; // disable Timer 2 interrupts
-//    ASSR = (1<<AS2); // select asynchronous operation of Timer2
-//    TCNT2 = 0; // clear Timer 2 counter
-//    TCCR2A = 0; //normal count up mode, no port output
-//    TCCR2B = (1<<CS22) | (1<<CS20); // select prescaler 128 => 1 sec between each overflow
-//
-//    while (ASSR & ((1<<TCN2UB)|(1<<TCR2BUB))); // wait for TCN2UB and TCR2BUB to clear
-//
-//    TIFR2 = 0xFF; // clear all interrupt flags
-//    TIMSK2 = (1<<TOIE2); // enable Timer2 overflow interrupt
-//}
 
 static void wdt_init()
 {
     wdt_reset();
-    WDTCSR = 0b00011000;
-    WDTCSR = 0b01001111;
+    WDTCR = 0b00011000;
+    WDTCR = 0b01001111;
 }
 
 ISR(WDT_vect)
 {
-//    fprintf(&uart_str, "interrupt\r\n");
 }
 
 static void wdt_sleep_2s()
 {
-    WDTCSR = 0b00011000;
-    WDTCSR = 0b01001111;
+    WDTCR = 0b00011000;
+    WDTCR = 0b01001111;
     sleep_cpu();
 }
 
 static void wdt_sleep_64ms()
 {
-    WDTCSR = 0b00011000;
-    WDTCSR = 0b01000011;
+    WDTCR = 0b00011000;
+    WDTCR = 0b01000011;
     sleep_cpu();
 }
 
 static void sys_init()
 {
     cli();
-    reset_enable_pullup();
-    led_enable_pin();
+    gpio_init();
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
     wdt_init();
-//    uart_init();
     sei();
 }
 
-static void led_0_on()  { PORTD |= _BV(PD6); }
-static void led_0_off() { PORTD &= ~_BV(PD6); }
+static void led_pulse(uint8_t mask)
+{
+    PORTB = mask;
+    _delay_ms(2);
+    PORTB = 0;
+    wdt_sleep_64ms();
+}
 
-static void led_1_on()  { PORTB |= _BV(PB3); }
-static void led_1_off() { PORTB &= ~_BV(PB3); }
+static void led_spark_if_dark()
+{
+//    if(PINB & 0b00000100)
+//        return; //dark
 
-static void led_2_on()  { PORTD |= _BV(PD5); }
-static void led_2_off() { PORTD &= ~_BV(PD5); }
-
-static void led_3_on()  { PORTD |= _BV(PD3); }
-static void led_3_off() { PORTD &= ~_BV(PD3); }
+    led_pulse(0b00000001);
+    led_pulse(0b00000010);
+    led_pulse(0b00001000);
+    led_pulse(0b00010000);
+    led_pulse(0b00000001);
+}
 
 int main(void) {
     sys_init();
-//    fprintf(&uart_str, "main\r\n");
+
     while(1) {
-        led_0_on();
-        _delay_ms(2);
-        led_0_off();
-        wdt_sleep_64ms();
-        led_1_on();
-        _delay_ms(2);
-        led_1_off();
-        wdt_sleep_64ms();
-        led_2_on();
-        _delay_ms(2);
-        led_2_off();
-        wdt_sleep_64ms();
-        led_3_on();
-        _delay_ms(2);
-        led_3_off();
-        wdt_sleep_64ms();
-        led_0_on();
-        _delay_ms(2);
-        led_0_off();
+        led_spark_if_dark();
         wdt_sleep_2s();
-//        fprintf(&uart_str, "wake\r\n");
     }
     return 0;
 }
